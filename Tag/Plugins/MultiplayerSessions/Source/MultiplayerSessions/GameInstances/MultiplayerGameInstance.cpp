3861d23f931c47102fee7df50b5e9e364bd31c95
Rajan117 <77441871+Rajan117@users.noreply.github.com>
2024-09-02 07:04:00 +0100

    Implemented Potential Invite Fix
    Used different delegate and removed unused delegates to simplify code.
    Needs testing.

--- Tag/Plugins/MultiplayerSessions/Source/MultiplayerSessions/GameInstances/MultiplayerGameInstance.cpp
+++ Tag/Plugins/MultiplayerSessions/Source/MultiplayerSessions/GameInstances/MultiplayerGameInstance.cpp
@@ -8,81 +8,31 @@
 
 UMultiplayerGameInstance::UMultiplayerGameInstance(const FObjectInitializer& ObjectInitializer) 
 	: Super(ObjectInitializer)
-	, SessionInviteReceivedDelegate(FOnSessionInviteReceivedDelegate::CreateUObject(this, &ThisClass::OnSessionInviteReceivedMaster))
-	, SessionInviteAcceptedDelegate(FOnSessionUserInviteAcceptedDelegate::CreateUObject(this, &ThisClass::OnSessionInviteAcceptedMaster))
-	, PlayerLoginChangedDelegate(FOnLoginChangedDelegate::CreateUObject(this, &ThisClass::OnPlayerLoginChangedMaster))
-	, PlayerLoginStatusChangedDelegate(FOnLoginStatusChangedDelegate::CreateUObject(this, &ThisClass::OnPlayerLoginStatusChangedMaster))
 {
-	MultiplayerSessionsSubsystem = GetSubsystem<UMultiplayerSessionsSubsystem>();
 }
 
 void UMultiplayerGameInstance::Shutdown()
 {
-	const IOnlineSubsystem* OnlineSubsystem = IOnlineSubsystem::Get();
-	if (!OnlineSubsystem) return;
-
-	if (const IOnlineSessionPtr SessionInterface = OnlineSubsystem->GetSessionInterface(); SessionInterface.IsValid())
-	{
-		SessionInterface->ClearOnSessionUserInviteAcceptedDelegate_Handle(SessionInviteAcceptedDelegateHandle);
-		SessionInterface->ClearOnSessionInviteReceivedDelegate_Handle(SessionInviteReceivedDelegateHandle);
-	}
-
-	if (const IOnlineIdentityPtr IdentityInterface = OnlineSubsystem->GetIdentityInterface(); IdentityInterface.IsValid())
-	{
-		IdentityInterface->ClearOnLoginChangedDelegate_Handle(PlayerLoginChangedDelegateHandle);
-		//Defaults to player 1
-		IdentityInterface->ClearOnLoginStatusChangedDelegate_Handle(0, PlayerLoginStatusChangedDelegateHandle);
-	}
-	
 	Super::Shutdown();
 }
 
 void UMultiplayerGameInstance::Init()
 {
 	Super::Init();
-	UKismetSystemLibrary::PrintString(this, "Game Instance Init");
-
 	const IOnlineSubsystem* OnlineSubsystem = IOnlineSubsystem::Get();
 	if (!OnlineSubsystem) return;
+	MultiplayerSessionsSubsystem = GetSubsystem<UMultiplayerSessionsSubsystem>();
 	
 	if (const IOnlineSessionPtr SessionInterface = OnlineSubsystem->GetSessionInterface(); SessionInterface.IsValid())
 	{
-		SessionInviteAcceptedDelegateHandle = SessionInterface->AddOnSessionUserInviteAcceptedDelegate_Handle(SessionInviteAcceptedDelegate);
-		SessionInviteReceivedDelegateHandle = SessionInterface->AddOnSessionInviteReceivedDelegate_Handle(SessionInviteReceivedDelegate);
-	}
-
-	if (const IOnlineIdentityPtr IdentityInterface = OnlineSubsystem->GetIdentityInterface(); IdentityInterface.IsValid())
-	{
-		PlayerLoginChangedDelegateHandle = IdentityInterface->AddOnLoginChangedDelegate_Handle(PlayerLoginChangedDelegate);
-		//Defaults to player 1
-		PlayerLoginStatusChangedDelegateHandle = IdentityInterface->AddOnLoginStatusChangedDelegate_Handle(0, PlayerLoginStatusChangedDelegate);
+		SessionInterface->OnSessionUserInviteAcceptedDelegates.AddUObject(this, &UMultiplayerGameInstance::OnSessionUserInviteAccepted);
 	}
 }
 
-void UMultiplayerGameInstance::OnSessionInviteReceivedMaster(const FUniqueNetId& PersonInvited,
-	const FUniqueNetId& PersonInviting, const FString& AppId, const FOnlineSessionSearchResult& SessionToJoin)
+void UMultiplayerGameInstance::OnSessionUserInviteAccepted(const bool bWasSuccessful, int32 LocalPlayerNum, FUniqueNetIdPtr PersonInvited,
+	const FOnlineSessionSearchResult& SessionToJoin)
 {
-	UKismetSystemLibrary::PrintString(this, "Invite Received");
-}
-
-void UMultiplayerGameInstance::OnSessionInviteAcceptedMaster(const bool bWasSuccessful, int32 LocalPlayer,
-	TSharedPtr<const FUniqueNetId> PersonInviting, const FOnlineSessionSearchResult& SessionToJoin)
-{
-	UKismetSystemLibrary::PrintString(this, "Attempted To Accept Invite");
-
-	if (bWasSuccessful)
-	{
-		MultiplayerSessionsSubsystem->JoinSession(SessionToJoin);
-	}
-}
-
-void UMultiplayerGameInstance::OnPlayerLoginChangedMaster(int32 PlayerNum)
-{
-
-}
-
-void UMultiplayerGameInstance::OnPlayerLoginStatusChangedMaster(int32 PlayerNum, ELoginStatus::Type PreviousStatus,
-	ELoginStatus::Type NewStatus, const FUniqueNetId& NewPlayerUniqueNetID)
-{
-
+	UKismetSystemLibrary::PrintString(this, "Accepted Invite");
+	if (!MultiplayerSessionsSubsystem) return;
+	MultiplayerSessionsSubsystem->JoinSession(SessionToJoin);
 }
