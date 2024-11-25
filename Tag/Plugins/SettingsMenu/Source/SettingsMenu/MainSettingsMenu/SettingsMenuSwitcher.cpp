30f688f50a862fda0300d4e91d51aa965f93ed0c
Rajan117 <77441871+Rajan117@users.noreply.github.com>
2024-11-25 09:06:36 +0000

    .

--- Tag/Plugins/SettingsMenu/Source/SettingsMenu/MainSettingsMenu/SettingsMenuSwitcher.cpp
+++ Tag/Plugins/SettingsMenu/Source/SettingsMenu/MainSettingsMenu/SettingsMenuSwitcher.cpp
@@ -2,3 +2,50 @@
 
 
 #include "SettingsMenuSwitcher.h"
+
+#include "UnrealWidgetFwd.h"
+#include "Components/Button.h"
+#include "Components/WidgetSwitcher.h"
+
+
+void USettingsMenuSwitcher::NativeConstruct()
+{
+	Super::NativeConstruct();
+	
+	if (GraphicsTabButton)
+	{
+		GraphicsTabButton->OnClicked.AddDynamic(this, &USettingsMenuSwitcher::OnGraphicsTabButtonClicked);
+	}
+	if (GameplayTabButton)
+	{
+		GameplayTabButton->OnClicked.AddDynamic(this, &USettingsMenuSwitcher::OnGameplayTabButtonClicked);
+	}
+	if (ControlsTabButton)
+	{
+		ControlsTabButton->OnClicked.AddDynamic(this, &USettingsMenuSwitcher::OnControlsTabButtonClicked);
+	}
+	if (KeybindsTabButton)
+	{
+		KeybindsTabButton->OnClicked.AddDynamic(this, &USettingsMenuSwitcher::OnKeybindsTabButtonClicked);
+	}
+}
+
+void USettingsMenuSwitcher::OnGraphicsTabButtonClicked()
+{
+	SettingsTabSwitcher->SetActiveWidgetIndex(0);
+}
+
+void USettingsMenuSwitcher::OnGameplayTabButtonClicked()
+{
+	SettingsTabSwitcher->SetActiveWidgetIndex(1);
+}
+
+void USettingsMenuSwitcher::OnControlsTabButtonClicked()
+{
+	SettingsTabSwitcher->SetActiveWidgetIndex(2);
+}
+
+void USettingsMenuSwitcher::OnKeybindsTabButtonClicked()
+{
+	SettingsTabSwitcher->SetActiveWidgetIndex(3);
+}
