098fae7940376477d2fcf383ed3b09e1e3772f7a
Rajan117 <77441871+Rajan117@users.noreply.github.com>
2024-12-29 19:15:23 +0000

    Removed 5.1 include order

--- Tag 5.3/Source/Tag.Target.cs
+++ Tag 5.3/Source/Tag.Target.cs
@@ -9,7 +9,6 @@ public class TagTarget : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
 		ExtraModuleNames.Add("Tag");
 	}
 }
