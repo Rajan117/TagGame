b17980e7dccc0387d0cd049b757fcdd7bc1c4e7b
Rajan117 <77441871+Rajan117@users.noreply.github.com>
2025-01-01 20:49:36 +0000

    Added settngs box

--- Tag 5.3/Plugins/SettingsMenu/Source/SettingsMenu/SettingsMenuTabs/SettingsMenuTab.cpp
+++ Tag 5.3/Plugins/SettingsMenu/Source/SettingsMenu/SettingsMenuTabs/SettingsMenuTab.cpp
@@ -16,6 +16,19 @@ void USettingsMenuTab::NativeConstruct()
 
 void USettingsMenuTab::Init()
 {
+	for (UWidget* Child : SettingsBox->GetAllChildren())
+	{
+		if (USetting* Setting = Cast<USetting>(Child))
+		{
+			Settings.Add(Setting);
+		}
+	}
+
+	for (const auto Setting : Settings)
+	{
+		Setting->Init(this);
+	}
+
 	LoadSettings();
 }
 
