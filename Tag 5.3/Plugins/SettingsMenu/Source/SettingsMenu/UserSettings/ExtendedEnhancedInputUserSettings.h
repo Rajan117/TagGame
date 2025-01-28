78595c0ce64fefd7b8edbc7aa9dc302f65d22910
Rajan117 <77441871+Rajan117@users.noreply.github.com>
2025-01-28 08:28:57 +0000

    Added EIUS class
    Custome user settings class to add aim sensitivity.

---
+++ Tag 5.3/Plugins/SettingsMenu/Source/SettingsMenu/UserSettings/ExtendedEnhancedInputUserSettings.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UserSettings/EnhancedInputUserSettings.h"
+#include "ExtendedEnhancedInputUserSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SETTINGSMENU_API UExtendedEnhancedInputUserSettings : public UEnhancedInputUserSettings
+{
+	GENERATED_BODY()
+};
