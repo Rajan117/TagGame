824fd4abcb29f48609581c681c271886f82c6496
Rajan117 <77441871+Rajan117@users.noreply.github.com>
2025-03-05 18:49:13 +0000

    Added Audio Settings Tab To UI
    Implemented the audio tab into the settings menu switched BP.
    Issues where audio settings are not loading.

--- Tag 5.3/Plugins/SettingsMenu/Source/SettingsMenu/SettingsMenuTabs/AudioSettingsMenu/AudioSetting.cpp
+++ Tag 5.3/Plugins/SettingsMenu/Source/SettingsMenu/SettingsMenuTabs/AudioSettingsMenu/AudioSetting.cpp
@@ -21,16 +21,27 @@ void UAudioSetting::NativeConstruct()
 void UAudioSetting::LoadSetting()
 {
 	Super::LoadSetting();
-	AudioSaveGameInstance = Cast<UAudioSettingsSaveGame>(UGameplayStatics::CreateSaveGameObject(UAudioSettingsSaveGame::StaticClass()));
-	UKismetSystemLibrary::PrintString(this, TEXT("Loading Audio Setting"));
+	if (UGameplayStatics::DoesSaveGameExist(TEXT("AudioSettingsSlot"), 0))
+	{
+		AudioSaveGameInstance = Cast<UAudioSettingsSaveGame>(UGameplayStatics::LoadGameFromSlot(TEXT("AudioSettingsSlot"), 0));
+		UKismetSystemLibrary::PrintString(this, TEXT("Audio Save Game Loaded"));
+	}
+	else
+	{
+		AudioSaveGameInstance = Cast<UAudioSettingsSaveGame>(UGameplayStatics::CreateSaveGameObject(UAudioSettingsSaveGame::StaticClass()));
+		UKismetSystemLibrary::PrintString(this, TEXT("Audio Save Game Not Found"));
+	}
 	if (AudioSaveGameInstance)
 	{
+		
 		if (AudioSaveGameInstance->VolumeSettings.Contains(VolumeNameText->GetText().ToString()))
 		{
+			UKismetSystemLibrary::PrintString(this, TEXT("Volume Setting Found"));
 			VolumeSlider->SetValue(AudioSaveGameInstance->VolumeSettings[VolumeNameText->GetText().ToString()]);
 		}
 		else
 		{
+			UKismetSystemLibrary::PrintString(this, TEXT("Volume Setting Not Found"));
 			VolumeSlider->SetValue(1.f);
 		}
 	}
@@ -45,8 +56,10 @@ void UAudioSetting::SaveSetting()
 	}
 	if (AudioSaveGameInstance)
 	{
+		UKismetSystemLibrary::PrintString(this, FString::Printf(TEXT("Saving Volume Setting: %s = %f"), *VolumeNameText->GetText().ToString(), VolumeSlider->GetValue()));
 		AudioSaveGameInstance->VolumeSettings.Add(VolumeNameText->GetText().ToString(), VolumeSlider->GetValue());
 		UGameplayStatics::SaveGameToSlot(AudioSaveGameInstance, TEXT("AudioSettingsSlot"), 0);
+		UKismetSystemLibrary::PrintString(this, TEXT("Audio Setting Saved"));
 	}
 }
 
