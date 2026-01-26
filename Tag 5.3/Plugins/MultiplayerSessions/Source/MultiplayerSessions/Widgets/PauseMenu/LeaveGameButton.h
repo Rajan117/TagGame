02829dfeee42204e65e5765ae6ef116e189466ea
Rajan117 <77441871+Rajan117@users.noreply.github.com>
2026-01-26 07:57:11 +0000

    Fixed quit button
    Added correct bindings.

--- Tag 5.3/Plugins/MultiplayerSessions/Source/MultiplayerSessions/Widgets/PauseMenu/LeaveGameButton.h
+++ Tag 5.3/Plugins/MultiplayerSessions/Source/MultiplayerSessions/Widgets/PauseMenu/LeaveGameButton.h
@@ -21,8 +21,20 @@ public:
 	UButton* LeaveButton;
 	
 protected:
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UUserWidget> LoadingWidgetClass;
+	
+	UPROPERTY(EditDefaultsOnly)
+	FString StartMapAddress = FString("Game/Maps/GameStartMap");
+
+	
+	void ShowLoadingWidget();
+	
 	UFUNCTION()
 	void OnLeaveButtonClicked();
+	
+	UFUNCTION()
+	void OnDestroySession(bool bWasSuccessful);
 
 private:
 	UPROPERTY()
